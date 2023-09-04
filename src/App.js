6e917290b377f0545c592a3c275bbbc6c3d5bdae
imvsoni24 <imvsoni24@gmail.com>
2023-09-04 12:41:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
 
 function App() {
+   const [btcPrice, setBtcPrice] = useState(25000);
+   const [dogePrice, setDogePrice] = useState(0.75);
+   const [ripplePrice, setRipplePrice] = useState(1.5);
+   const [btcQuantity, setBtcQuantity] = useState("");
+   const [dogeQuantity, setDogeQuantity] = useState("");
+   const [rippleQuantity, setRippleQuantity] = useState("");
+   const [btcTotal, setBtcTotal] = useState(0);
+   const [dogeTotal, setDogeTotal] = useState(0);
+   const [rippleTotal, setRippleTotal] = useState(0);
+
+   const calculateTotal = () => {
+     setBtcTotal(btcPrice * btcQuantity);
+     setDogeTotal(dogePrice * dogeQuantity);
+     setRippleTotal(ripplePrice * rippleQuantity);
+   };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{display:"flex",justifyContent:"space-between"}}>
+        <div>
+          <h2>BTC</h2>
+          <p>${btcPrice}</p>
+          <input
+            type="number"
+            placeholder="QTY"
+            value={btcQuantity}
+            onChange={(e) => setBtcQuantity(e.target.value)}
+          />
+          <button onClick={calculateTotal}>Add</button>
+        </div>
+
+        <div>
+          <h2>DOGE</h2>
+          <p>${dogePrice}</p>
+          <input
+            type="number"
+            placeholder="QTY"
+            value={dogeQuantity}
+            onChange={(e) => setDogeQuantity(e.target.value)}
+          />
+          <button onClick={calculateTotal}>Add</button>
+        </div>
+        <div>
+          <h2>Ripple</h2>
+          <p>${ripplePrice}</p>
+          <input
+            type="number"
+            placeholder="QTY"
+            value={rippleQuantity}
+            onChange={(e) => setRippleQuantity(e.target.value)}
+          />
+          <button onClick={calculateTotal}>Add</button>
+        </div>
+      </div>
     </div>
   );
 }
